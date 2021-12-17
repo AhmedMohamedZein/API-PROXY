18bdd9ae16481639d420b69b43648400bce3831a
AhmedZein <ahmedzain503@gmail.com>
2021-12-17 16:38:02 +0200

    nothing here

--- Routes/weather.js
+++ Routes/weather.js
@@ -1,12 +1,10 @@
 const express = require ('express');
 const Routes = express.Router();
 const path = require ('path');
-
-
 require('dotenv').config({path : 'C:\Users\AhmedZein\Desktop\API-PROXY\.gitignore\.env'});
 
 Routes.get ('/' , ( req, res)=> {
-    res.sendFile(path.join (  process.env.weatherHtmlPage ) );
+    res.sendFile(path.join (  process.env.mainHtmlPage ) );
 });
 
 
